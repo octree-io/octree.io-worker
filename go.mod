1977f89b963ec19dd2c3a46af8ea9bd2ed18f7e3
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-10-15 17:15:59 -0500

    feat: Add OpenAI GPT-4o-mini model for Trivia grading

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.23.2
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/rabbitmq/amqp091-go v1.10.0
+	github.com/sashabaranov/go-openai v1.32.0
 )
--- internal/workers/trivia_worker.go
+++ internal/workers/trivia_worker.go
@@ -1,14 +1,43 @@
 package workers
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	ampq "github.com/rabbitmq/amqp091-go"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 func processTriviaGrading() {
-	time.Sleep(3 * time.Second)
+	prompt := `I want you to grade these answers for these questions. For each question, put either a Yes or No for whether or not it passes an interview or an exam. Explain in-depth what the right answer is supposed to be. Be strict about the grading to make sure that the explanations are correct. It is acceptable if there are no specific examples unless the question specifically asks for examples. Q: What is a thread? A: A thread is another instance of a program running within the same program.`
+
+	start := time.Now()
+
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT4oMini,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+
+	fmt.Printf("Response took %v to complete", time.Since(start))
 }
 
 func SpawnTriviaWorker(id int, msgs <-chan ampq.Delivery) {
